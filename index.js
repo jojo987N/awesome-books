f93622eb623f465f70027102e04a2c56a9c21173
nass <cabliveer@gmail.com>
2023-04-24 13:26:19 +0200

    Create awesome books: plain JavaScript with objects

---
+++ index.js
@@ -0,0 +1,54 @@
+class Store {
+  constructor(){
+    this.data = JSON.parse(localStorage.getItem('books')) || [];
+  }
+  add(book){
+    this.data.push(book)
+    localStorage.setItem('books', JSON.stringify(this.data));
+  }
+  remove(id){
+    this.data = this.data.filter((book) => book.id !== id)
+    localStorage.setItem('books', JSON.stringify(this.data));
+  }
+}
+var store = new Store()
+
+window.onload = () => {
+    const addButton = document.querySelector(".add")
+    const books = document.querySelector(".books")
+    const title = document.querySelector(".title")
+    const author = document.querySelector(".author")
+     
+    const article = (book) => `<article id=${book.id}>
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button class="remove">Remove</button>
+    </article>`
+
+    books.innerHTML = store.data.map((book) => article(book)).join("")
+    document.querySelectorAll(".remove").forEach((removeButton) => removeButton.onclick = (e) => remove(e))
+
+    addButton.onclick = () => {
+
+        let id = Math.floor(Math.random()*(999-100+1)+100);
+        let book = {
+            id,
+            title: title.value,
+            author: author.value
+        }
+        store.add(book)
+        var wrapper= document.createElement('div');
+        wrapper.innerHTML= article(book)
+        
+        var div= wrapper.firstChild;
+        
+        div.childNodes[5].onclick = (e) => remove(e)
+        
+        books.appendChild(div)
+    }
+}
+
+function remove(e){
+    e.target.parentElement.remove()
+    store.remove(parseInt(e.target.parentElement.id))
+}
